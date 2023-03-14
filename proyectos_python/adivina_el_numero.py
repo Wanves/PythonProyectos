8f33f3eb19ea2fedf19117b2d3a2d2ae911fa3fd
Juan C. Wanvestrant <107213869+Wanves@users.noreply.github.com>
2023-03-14 02:01:19 -0300

    segundo cambio

--- proyectos_python/adivina_el_numero.py
+++ proyectos_python/adivina_el_numero.py
@@ -19,6 +19,16 @@ def run():
             
             while prediccion != numero_aleatorio:
                 prediccion = int(input(f"Adivina un número entre 1 y {x}: "))
+                if prediccion < numero_aleatorio:
+                    print("Intenta nuevamente. Este número es muy pequeño.")
+                elif prediccion > numero_aleatorio:
+                    print("Intentalo nuevamente. Este número es muy grande.")
+                elif prediccion == numero_aleatorio:
+                    print(f"Felicidades! Adivinaste el número aleatorio.")
+                    print(f"Número aleatorio: {numero_aleatorio}\nNúmero seleccionado: {prediccion}")
+                else:
+                    print("Estas intentando ingresar un valor no valido.")
+            
             
         adivina_el_numero(10)
     except:
