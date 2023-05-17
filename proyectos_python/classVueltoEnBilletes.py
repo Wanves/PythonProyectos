4aba7124dbf4a39ca18281016492a0aa1535ff02
Juan C. Wanvestrant <107213869+Wanves@users.noreply.github.com>
2023-05-16 22:41:13 -0300

    Vuelto en billetes

---
+++ proyectos_python/classVueltoEnBilletes.py
@@ -0,0 +1,36 @@
+# EJERCICIO en Python 🐍 : class Vuelto en billetes 💵
+
+
+class Contadora:
+    def __init__(self,monto):
+        print(" ")
+        self.monto = int(input('Ingresa el monto en dólares: '))
+        print(" ")
+        
+        
+    def VueltoEnBilletes(self):
+        cant_100 = self.monto // 100
+        self.monto = self.monto % 100
+        cant_50 =  self.monto // 50
+        self.monto = self.monto % 50
+        cant_20 =  self.monto // 20
+        self.monto = self.monto % 20
+        cant_10 =  self.monto // 10
+        self.monto = self.monto % 10
+        cant_5 =  self.monto // 5
+        self.monto = self.monto % 5
+        cant_2 =  self.monto // 2
+        self.monto = self.monto % 2
+
+        print(f'La cantidad de billetes de 100: {cant_100}')
+        print(f'La cantidad de billetes de 50: {cant_50}')
+        print(f'La cantidad de billetes de 20: {cant_20}')
+        print(f'La cantidad de billetes de 10: {cant_10}')
+        print(f'La cantidad de billetes de 5: {cant_5}')
+        print(f'La cantidad de billetes de 2: {cant_2}')
+        print(f'La cantidad de billetes de 1: {self.monto}')
+        print(" ")
+
+
+contadora = Contadora(0)
+contadora.VueltoEnBilletes()
